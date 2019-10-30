2f90a217aa95276fdfade0d98952e3bdff3d7454
thanght1 <v.thanght1@vinsmart.net.vn>
2019-10-30 16:47:37 +0700

    Add sub item for menu

--- src/components/sider/Sider.js
+++ src/components/sider/Sider.js
@@ -4,7 +4,15 @@ import './Sider.css';
 
 const menuItems = [
   { icon: 'plus', name: 'Item 1', route: '1', default: true },
-  { icon: 'plus', name: 'Item 2', route: '2' },
+  {
+    icon: 'plus',
+    name: 'Item 2',
+    route: '2',
+    subItems: [
+      { icon: 'minus', name: 'Sub item 1' },
+      { icon: 'minus', name: 'Sub item 2' }
+    ]
+  },
   { icon: 'plus', name: 'Item 3', route: '3' },
   { icon: 'plus', name: 'Item 4', route: '4' },
   { icon: 'plus', name: 'Item 5', route: '5' }
@@ -35,12 +43,35 @@ class Sider extends Component {
           defaultSelectedKeys={defaultItems[0].route}
           mode="inline"
         >
-          {menuItems.map(item => (
-            <Menu.Item key={item.route}>
-              <Icon type={item.icon} />
-              <span>{item.name}</span>
-            </Menu.Item>
-          ))}
+          {menuItems.map(item => {
+            if (item.subItems && item.subItems.length > 0) {
+              return (
+                <Menu.SubMenu
+                  key={item.route}
+                  title={
+                    <span>
+                      <Icon type={item.icon} />
+                      <span>{item.name}</span>
+                    </span>
+                  }
+                >
+                  {item.subItems.map(subItem => (
+                    <Menu.Item key={subItem.route}>
+                      <Icon type={subItem.icon} />
+                      <span>{subItem.name}</span>
+                    </Menu.Item>
+                  ))}
+                </Menu.SubMenu>
+              );
+            } else {
+              return (
+                <Menu.Item key={item.route}>
+                  <Icon type={item.icon} />
+                  <span>{item.name}</span>
+                </Menu.Item>
+              );
+            }
+          })}
         </Menu>
       </Layout.Sider>
     );
